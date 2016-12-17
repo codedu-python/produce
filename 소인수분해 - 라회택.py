fdd79cb9d9446c59e69ba1a9439ffac9c878779f
hoetaekpro <hoetaekpro@hoetaekproui-MacBook-Pro.local>
2016-12-17 14:37:19 +0900

    최소공배수 완성

--- 소인수분해 - 라회택.py
+++ 소인수분해 - 라회택.py
@@ -1,27 +1,32 @@
 N = int(input("숫자를 입력하세요"))
-insu = []
-for i in range(1, N+1):
-    if N % i == 0:
-        insu.append(i)
-Num_prime = list(range(2, N))
-for n in Num_prime:
-    for compare in range(n+1, N):
-        if compare % n ==0 and compare in Num_prime:
-            Num_prime.remove(compare)
 
 
-soinsu = []
-for num in insu:
-    if num in Num_prime:
-        print(num)
-        count = 0
-        while True:
+def soinsubunhae(num):
+    insu = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            insu.append(i)
+    Num_prime = list(range(2, num))
+    for n in Num_prime:
+        for compare in range(n+1, num):
+            if compare % n ==0 and compare in Num_prime:
+                Num_prime.remove(compare)
 
-            print(count)
-            if N/num == int(N/num):
-                count += 1
-                N = int(N/num)
-            else:
-                break
-        soinsu.append((num, count))
-print(soinsu)
+
+    soinsu = []
+    for primeinsu in insu:
+        if primeinsu in Num_prime:
+            print(primeinsu)
+            count = 0
+            while True:
+
+                print(count)
+                if num/primeinsu == int(num/primeinsu):
+                    count += 1
+                    num = int(num/primeinsu)
+                else:
+                    break
+            soinsu.append((primeinsu, count))
+    return soinsu
+
+print(soinsubunhae(N))
--- 최대공약수 최소공배수 - 라회택.py
+++ 최대공약수 최소공배수 - 라회택.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 A = int(input('첫번째 숫자를 입력하세요'))
 B = int(input('두번째 숫자를 입력하세요'))
 
@@ -16,3 +18,38 @@ if max_insu != 1:
     print('두 수의 최대공약수는 ',max_insu)
 else:
     print('최대공약수 없음')
+
+
+def soinsubunhae(num):
+    insu = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            insu.append(i)
+    Num_prime = list(range(2, num))
+    for n in Num_prime:
+        for compare in range(n+1, num):
+            if compare % n ==0 and compare in Num_prime:
+                Num_prime.remove(compare)
+
+
+    soinsu = []
+    for primeinsu in insu:
+        if primeinsu in Num_prime:
+            count = 0
+            while True:
+                if num/primeinsu == int(num/primeinsu):
+                    count += 1
+                    num = int(num/primeinsu)
+                    soinsu.append(primeinsu)
+                else:
+                    break
+
+    return soinsu
+
+soinsu_A = Counter(soinsubunhae(A))
+soinsu_B = Counter(soinsubunhae(B))
+LCM = list((soinsu_A|soinsu_B).elements())
+multi = 1
+for number in LCM:
+    multi *= number
+print("두 수의 최소공배수는 ", multi)
